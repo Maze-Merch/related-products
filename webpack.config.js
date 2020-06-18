244c2e801e91e7fd6801b62a13d94409eb05f5ed
Matt Roberts <justskiit@gmail.com>
2020-06-18 15:57:11 -0600

    New package and webpack config to match team new dir structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,25 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  entry: './client/src/index.js',
+  entry: "./client/src/index.js",
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.html$/,
         use: [
-          {
-            loader: 'html-loader',
-          },
-        ],
-      },
-    ],
+          "babel-loader"
+        ]
+      }
+    ]
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx'],
+    extensions: [
+      "*", ".js", ".jsx"
+    ]
   },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: './client/src/index.html',
-      filename: './index.html',
-    }),
-  ],
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js"
+  }
 };
