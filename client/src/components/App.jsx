1e4c7ddb249c5e023752433a6bb17539a3936e11
Matt Roberts <justskiit@gmail.com>
2020-06-29 12:20:38 -0600

    Fixed bug where arrows would disappear after clicked

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -77,25 +77,25 @@ class App extends React.Component {
             slidesToShow: 3,
             slidesToScroll: 3,
             infinite: true,
-            dots: true
-          }
+            dots: true,
+          },
         },
         {
           breakpoint: 600,
           settings: {
             slidesToShow: 2,
             slidesToScroll: 2,
-            initialSlide: 2
-          }
+            initialSlide: 2,
+          },
         },
         {
           breakpoint: 480,
           settings: {
             slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        }
-      ]
+            slidesToScroll: 1,
+          },
+        },
+      ],
     };
     return (
       <div id="rpCarousel">
