fe223bec75719f73aa8a411690788a407f12e590
Matt Roberts <justskiit@gmail.com>
2020-07-01 20:20:54 -0600

    Code cleanup

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,8 @@
 /* eslint-disable class-methods-use-this */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import regeneratorRuntime from "regenerator-runtime";
+import regeneratorRuntime from 'regenerator-runtime';
 import MyOutfits from './MyOutfits';
-// import prodDetailsData from './exampleData/prodDetails.json';
-// import prodStylesData from './exampleData/prodStyles.json';
 import currentProduct from './exampleData/currentProduct.json';
 import Carousel from './Carousel';
 
@@ -27,9 +25,7 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    // this.getRelIds();
     this.dataFetcher();
-    // this.setState({ prodDetails: this.productDetailsArr, prodStyles: this.productStylesArr });
   }
 
   getRelIds() {
@@ -49,7 +45,7 @@ class App extends React.Component {
   getDetails(relIds) {
     const detailsPromises = [];
     for (let i = 0; i <= relIds.length - 1; i += 1) {
-      let id = relIds[i];
+      const id = relIds[i];
       const detailPromise = fetch(`http://52.26.193.201:3000/products/${id}`)
         .then((res) => res.json());
       detailsPromises.push(detailPromise);
@@ -60,9 +56,9 @@ class App extends React.Component {
   getStyles(relIds) {
     const stylesPromises = [];
     for (let i = 0; i <= relIds.length - 1; i += 1) {
-      let id = relIds[i];
+      const id = relIds[i];
       const stylePromise = fetch(`http://52.26.193.201:3000/products/${id}/styles`)
-        .then((res) => res.json())
+        .then((res) => res.json());
       stylesPromises.push(stylePromise);
     }
     return Promise.all(stylesPromises);
@@ -91,8 +87,8 @@ class App extends React.Component {
         <div id="rpCarousel">
           <h4>Related Products</h4>
           {
-            relatedProdIds ? <div className="rp-loading fa fa-spinner fa-pulse fa-3x fa-fw"><span className="sr-only">Loading...</span></div>
-              : <Carousel currProd={currentProd} det={prodDetails} style={this.productStylesArr} />
+            relatedProdIds ? <div className="fa fa-refresh fa-spin fa-3x fa-fw rp-loading"><span className="sr-only">Loading...</span></div>
+              : <Carousel currProd={currentProd} det={prodDetails} style={prodStyles} />
           }
         </div>
         <div id="myOutfits">
