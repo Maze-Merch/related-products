a2ca28f93ecb5d15390559b853e00bc65297f20a
Matt Roberts <justskiit@gmail.com>
2020-08-12 09:52:34 -0600

    Updated star rating component styles

--- client/src/components/card/CardBody.jsx
+++ client/src/components/card/CardBody.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import Rating from '@material-ui/lab/Rating';
 
 const CardBody = ({name, slogan, desc, cat, price, stName, salePr}) => {
   // console.log('stName', price);
   let ucCat = cat.toUpperCase();
+
+  const starNum = () => (Math.floor(Math.random() * 5) + 1);
+
   return (
 
     <div>
@@ -10,7 +14,9 @@ const CardBody = ({name, slogan, desc, cat, price, stName, salePr}) => {
       <h5>{name}: <br /> {stName}</h5>
       <p className="rp-card-text">{slogan}</p>
       <div className="rp-card-price">$ {price}</div>
-      <div className="rp-card-stars">*****</div>
+      <div className="rp-card-stars">
+        <Rating name="read-only" defaultValue={starNum()} size="small" />
+      </div>
     </div>
   );
 };
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const port = 3002;
 
 const app = express();
 
@@ -9,4 +10,6 @@ app.use(cors());
 // creates routes for anything located in the directory we've pointed it to
 app.use(express.static('public'));
 
-app.listen(3002);
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
